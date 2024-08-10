d8216320f559f52337185dabe9ab3a5b4a0c8973
Rustem Kakimov <ruskakimov@gmail.com>
2024-08-10 19:59:58 -0400

    Timer

--- app/src/main/java/com/example/yogabook/DetailsActivity.kt
+++ app/src/main/java/com/example/yogabook/DetailsActivity.kt
@@ -2,6 +2,7 @@ package com.example.yogabook
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -9,8 +10,27 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Timer
+import java.util.TimerTask
 
 class DetailsActivity : AppCompatActivity() {
+    // Timer state
+    val timer = Timer()
+    var elapsedSeconds = 0
+
+    // Intent data
+    var poseName = ""
+    var imageResId = 0
+    var maxDuration = 10
+
+    // UI elements
+    lateinit var toolbar: Toolbar
+    lateinit var titleTextView: TextView
+    lateinit var difficultyTextView: TextView
+    lateinit var durationTextView: TextView
+    lateinit var poseImageView: ImageView
+    lateinit var playButton: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,21 +42,41 @@ class DetailsActivity : AppCompatActivity() {
         }
 
         // Retrieve data from the intent
-        val poseName = intent.getStringExtra("poseName")
-        val imageResId = intent.getIntExtra("poseImageResId",0)
+        poseName = intent.getStringExtra("poseName") ?: "?"
+        imageResId = intent.getIntExtra("poseImageResId",0)
 
         // UI elements
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        val decorTitle: TextView = findViewById(R.id.title)
-        val poseImage: ImageView = findViewById(R.id.pose_image)
+        toolbar = findViewById(R.id.toolbar)
+        titleTextView = findViewById(R.id.title)
+        difficultyTextView = findViewById(R.id.difficulty)
+        durationTextView = findViewById(R.id.duration)
+        poseImageView = findViewById(R.id.pose_image)
+        playButton = findViewById(R.id.imageButton)
 
         // Set up the toolbar
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowTitleEnabled(true)
 
-        decorTitle.text = poseName
-        poseImage.setImageResource(imageResId)
+        // Update UI data
+        titleTextView.text = poseName
+        poseImageView.setImageResource(imageResId)
+        playButton.setOnClickListener { onPlayButtonClick() }
+    }
+
+    private fun onPlayButtonClick() {
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                elapsedSeconds++
+                runOnUiThread {
+                    // Update the UI here
+                    durationTextView.text = "Time left – ${maxDuration - elapsedSeconds} sec"
+                }
+                if (elapsedSeconds >= maxDuration) {
+                    timer.cancel()
+                }
+            }
+        }, 0, 1000)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
