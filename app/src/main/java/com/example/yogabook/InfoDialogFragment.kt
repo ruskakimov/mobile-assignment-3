d10c19455a38e857002043a1e62623aef80f23e1
Rustem Kakimov <ruskakimov@gmail.com>
2024-08-11 20:26:34 -0400

    Change content

--- app/src/main/java/com/example/yogabook/InfoDialogFragment.kt
+++ app/src/main/java/com/example/yogabook/InfoDialogFragment.kt
@@ -10,8 +10,20 @@ class InfoDialogFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
             val builder = AlertDialog.Builder(it)
-            builder.setTitle("Your Dialog Title")
-                .setMessage("Your dialog message.")
+            builder.setTitle("YogaBook - How It Works")
+                .setMessage("1. Browse Yoga Poses:\n" +
+                        "Explore the list of yoga poses. Each pose is accompanied by a helpful image to guide you.\n" +
+                        "\n" +
+                        "2. Select a Pose:\n" +
+                        "Tap on a pose to start the timer. This will begin your focused session on that particular pose.\n" +
+                        "\n" +
+                        "3. Timer & Notification:\n" +
+                        "The timer will count down the duration for holding the pose. When the timer ends, you'll receive a gentle notification letting you know that the time is up.\n" +
+                        "\n" +
+                        "4. Rest or Continue:\n" +
+                        "After the timer ends, you have the option to take a short rest or move directly to the next pose. Simply select your next pose to start the timer again.\n" +
+                        "\n" +
+                        "Stay mindful and enjoy your practice!")
                 .setPositiveButton("OK") { dialog, _ ->
                     dialog.dismiss()
                 }
